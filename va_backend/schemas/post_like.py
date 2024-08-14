a02dbc16a7f6bcf5fa11757f38571522c354f00b
thisisteey <dadataiwo82@yahoo.com>
2024-08-14 17:53:42 +0100

    Fix: Correct __table_args__ format in post_like schema

--- va_backend/schemas/post_like.py
+++ va_backend/schemas/post_like.py
@@ -10,7 +10,12 @@ class PostLike(BaseModel, Base):
     """PostLike model class for like on a post"""
     __tablename__ = 'posts_likes'
     __table_args__ = (
-        UniqueConstraint('post_id', 'user_id', name='unique_reaction'))
+        UniqueConstraint(
+            'post_id',
+            'user_id',
+            name='unique_reaction'
+        ),
+    )
     id = Column(String(64), unique=True, nullable=False, primary_key=True)
     created_on = Column(TIMESTAMP(True), nullable=False,
                         default=datetime.utcnow())
